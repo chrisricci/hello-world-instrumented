5219a11d77c58618581707d2f0d6904614524503
Chris Ricci <riccic@Chriss-MacBook-Pro.local>
2017-06-01 13:47:11 -0400

    removing '-' from env vars

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import sys
 from flask import Flask
 
 app = Flask(__name__)
-greeterUrl = os.environ.get('GREETER-SERVICE') if os.environ.get('GREETER-SERVICE') != None else 'http://localhost:8080'
-sys.stdout.write('GREETER-SERVICE: ' + greeterUrl)
+greeterUrl = os.environ.get('GREETERSERVICE') if os.environ.get('GREETERSERVICE') != None else 'http://localhost:8080'
+sys.stdout.write('GREETERSERVICE: ' + greeterUrl)
 
-nameserviceUrl = os.environ.get('NAME-SERVICE') if os.environ.get('NAME-SERVICE') != None else 'http://localhost:8081'
-sys.stdout.write('NAME-SERVICE: ' + nameserviceUrl)
+nameserviceUrl = os.environ.get('NAMESERVICE') if os.environ.get('NAMESERVICE') != None else 'http://localhost:8081'
+sys.stdout.write('NAMESERVICE: ' + nameserviceUrl)
 
 @app.route('/')
 def index():
