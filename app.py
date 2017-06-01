1c9319b9c98077fc401ae1c36f9816ccf1b5de46
Chris Ricci <riccic@Chriss-MacBook-Pro.local>
2017-06-01 16:25:22 -0400

    adding prom metrics

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import sys
 import logging
 
 from flask import Flask
+from prometheus_client import Summary, Counter
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -16,6 +17,8 @@ app.logger.debug('GREETERSERVICE: ' + greeterUrl)
 nameserviceUrl = os.environ.get('NAMESERVICE') if os.environ.get('NAMESERVICE') != None else 'http://localhost:8081'
 app.logger.debug('NAMESERVICE: ' + nameserviceUrl)
 
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+@REQUEST_TIME.time()
 @app.route('/')
 def index():
     # Call Greeter Service
@@ -29,3 +32,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80,debug=True)
+    start_http_server(8000)
