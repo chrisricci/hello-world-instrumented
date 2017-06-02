059aa10c5eee2993e11675b4e4342a15db27b99a
Chris Ricci <riccic@Chriss-MacBook-Pro.local>
2017-06-02 10:42:10 -0400

    adding prom monitoring, updating port

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import sys
 import logging
 
 from flask import Flask
-from prometheus_client import Summary, Counter
+from flask_prometheus import monitor
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -17,8 +17,6 @@ app.logger.debug('GREETERSERVICE: ' + greeterUrl)
 nameserviceUrl = os.environ.get('NAMESERVICE') if os.environ.get('NAMESERVICE') != None else 'http://localhost:8081'
 app.logger.debug('NAMESERVICE: ' + nameserviceUrl)
 
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
-@REQUEST_TIME.time()
 @app.route('/')
 def index():
     # Call Greeter Service
@@ -31,5 +29,5 @@ def index():
     return "%s, %s!" % (greeterResponse.text, nameserviceResponse.text)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80,debug=True)
-    start_http_server(8000)
+    monitor(app, port=8000)
+    app.run(host='0.0.0.0')
