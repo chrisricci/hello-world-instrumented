967123e758c46bcc7beacb09fd7813ea2bc9e0b8
Chris Ricci <chris.ricci@coreos.com>
2017-10-27 12:08:42 -0400

    reverting

--- app.py
+++ app.py
@@ -28,11 +28,11 @@ def index():
       # Force a sleep.
       # Comment this for demo purposes
       app.logger.debug("Forcing a sleep")
-      time.sleep(2)
+      # time.sleep(2)
 
     timestamp2 = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     app.logger.debug("Finished at: " + timestamp2)
-    return timestamp2 + " Hello, World!! - Updated!!!!\n"
+    return timestamp2 + " Hello, World!!\n"
 
 if __name__ == '__main__':
     monitor(app, port=8000)
