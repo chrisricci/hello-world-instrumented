09828873046f25ede15248245aeb950db9c2c765
cricci82 <chris.ricci82@gmail.com>
2018-10-17 12:27:47 -0400

    update

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def index():
     # time.sleep(2)
     timestamp2 = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     app.logger.debug("Finished at: " + timestamp2)
-    return timestamp2 + " " + hostname + " Hello, World!! - Update\n"
+    return timestamp2 + " " + hostname + " Hello, World!!\n"
 
 if __name__ == '__main__':
     monitor(app, port=8000)
